8f83d2d3ebfa4e3fc8f921621fa24ecd755a9197
Wollivan <tim.smith2814@gmail.com>
2022-07-06 14:03:02 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./Header";
 import Parent from "./Parent";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ margin: "1rem" }}>
+      <Header />
       <h1>State and Props</h1>
       <Parent />
     </div>
--- src/Child.js
+++ src/Child.js
@@ -1,5 +1,12 @@
 import { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import boy from "./assets/boy.jpeg";
+import Button from "react-bootstrap/Button";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default class Child extends Component {
   constructor(props) {
@@ -9,9 +16,11 @@ export default class Child extends Component {
     };
   }
   patBillysHead = () => {
-    this.setState({
-      patsOnBillysHead: this.state.patsOnBillysHead + 1,
-    });
+    if (this.state.patsOnBillysHead < 50) {
+      this.setState({
+        patsOnBillysHead: this.state.patsOnBillysHead + 1,
+      });
+    }
     console.log("Patting billys head");
   };
 
@@ -26,7 +35,22 @@ export default class Child extends Component {
           title={this.props.name}
           width={300}
         />
-        <span>😋👋 {this.state.patsOnBillysHead}</span>
+        <Container>
+          <Row>
+            <Col>
+              <Button variant="success">Success</Button>
+            </Col>
+            <Col>
+              <span>😋👋 {this.state.patsOnBillysHead}</span>
+            </Col>
+            <Col>
+              <ProgressBar
+                now={this.state.patsOnBillysHead * 2}
+                label={`${this.state.patsOnBillysHead * 2}%`}
+              />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
